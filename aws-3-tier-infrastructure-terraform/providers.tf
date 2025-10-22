8ed431f85fc1e479a9eb83d62a0ea9a576f443b8
mohamedonthecloud <mohamedoabdulkadir@gmail.com>
2025-10-22 20:58:54 +0100

    feat: adding secret key and access key provider values and variables

--- aws-3-tier-infrastructure-terraform/providers.tf
+++ aws-3-tier-infrastructure-terraform/providers.tf
@@ -11,4 +11,6 @@ terraform {
 # AWS Provider Configuration
 provider "aws" {
   region = "eu-west-2"
+  access_key = var.access_key
+  secret_key = var.secret_access_key
 }
--- aws-3-tier-infrastructure-terraform/variables.tf
+++ aws-3-tier-infrastructure-terraform/variables.tf
@@ -1,4 +1,14 @@
 variable "environment_name" {
-      description = "Name of the environment (e.g., dev, prod)"
-      type        = string
-    }
+    description = "Name of the environment (e.g., dev, prod)"
+    type        = string
+}
+
+variable "access_key" {
+    description = "Name of public access key"
+    type        = string
+}
+
+variable "secret_access_key" {
+    description = "Name of private access key"
+    type        = string
+}
